42b0e2df5de363589ee28a2e6a3f089c7de90383
seo hyeon ha <hyeonha1103@gmail.com>
2025-12-31 19:07:26 +0900

    gg

---
+++ src/main/java/org/example/Head02_JavaProgramming/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.example.Head02_JavaProgramming;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java현하" +
+                "!");
+    }
+}
+
+
