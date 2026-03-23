66eb93790be76d965d9b26fb6fcef60aeb0ea085
seo hyeon ha <hyeonha1103@gmail.com>
2026-03-23 18:48:04 +0900

    2026-03-23

---
+++ SpringStableHigh/src/main/java/com/sprint/mission/springstablehigh/ex1/UserController.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.springstablehigh.ex1;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping
+  public String createUser(@RequestParam String email) {
+    userService.register(email);
+    return "회원가입 성공";
+  }
+}
