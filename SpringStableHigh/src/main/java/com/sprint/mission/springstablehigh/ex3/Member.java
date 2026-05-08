27128522684140d7811be8b7c09c09687370d5c4
seo hyeon ha <hyeonha1103@gmail.com>
2026-05-08 15:51:22 +0900

    2026-05-08

---
+++ SpringStableHigh/src/main/java/com/sprint/mission/springstablehigh/ex3/Member.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.springstablehigh.ex3;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+}
---
+++ SpringStableHigh/src/main/java/com/sprint/mission/springstablehigh/ex3/MemberController.java
@@ -0,0 +1,30 @@
+package com.sprint.mission.springstablehigh.ex3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+  private final MemberRepository memberRepository;
+
+  @PostMapping
+  public Member create(@RequestParam String name) {
+    return memberRepository.save(
+        Member.builder()
+            .name(name)
+            .build()
+    );
+  }
+
+  @GetMapping("/count")
+  public long count() {
+    return memberRepository.count();
+  }
+}
