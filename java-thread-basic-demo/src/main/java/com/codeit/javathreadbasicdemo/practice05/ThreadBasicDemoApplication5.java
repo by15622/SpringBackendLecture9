67bec1cb6f76c681b77cb23d274fedd21c7791cf
seo hyeon ha <hyeonha1103@gmail.com>
2026-05-26 19:03:10 +0900

    2026-05-26

---
+++ java-thread-basic-demo/src/main/java/com/codeit/javathreadbasicdemo/practice05/ThreadBasicDemoApplication5.java
@@ -0,0 +1,13 @@
+package com.codeit.javathreadbasicdemo.practice05;
+
+import com.codeit.javathreadbasicdemo.practice01.SyncAsyncComparisonDemo;
+
+public class ThreadBasicDemoApplication5 {
+
+  public static void main(String[] args) {
+    System.out.println("Java Thread 기초와 동기화 실습 시작");
+    SyncAsyncComparisonDemo.run();
+    System.out.println("실습 종료");
+  }
+
+}
