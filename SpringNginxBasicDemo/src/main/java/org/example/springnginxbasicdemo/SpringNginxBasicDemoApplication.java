6ced583b1ab21c288fd6e1c3de4884b6c89b6cea
seo hyeon ha <hyeonha1103@gmail.com>
2026-06-15 13:35:31 +0900

    2026-06-15

---
+++ SpringNginxBasicDemo/src/main/java/org/example/springnginxbasicdemo/SpringNginxBasicDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springnginxbasicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNginxBasicDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringNginxBasicDemoApplication.class, args);
+  }
+
+}
---
+++ SpringNginxBasicDemo/src/test/java/org/example/springnginxbasicdemo/SpringNginxBasicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springnginxbasicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNginxBasicDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
